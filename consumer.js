1809e61206a0e8fa24f6985ee29a70f1b1e696bb
Tushar Singh <73421331+toxicanthakur@users.noreply.github.com>
2023-01-27 17:30:10 +0530

    initial commit

---
+++ consumer.js
@@ -0,0 +1,21 @@
+const amqp = require("amqplib");
+connect();
+async function connect() {
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672")
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.consume("jobs", message =>{
+            const input = JSON.parse(message.content.toString());
+            console.log(`Recieved job with input ${input.number}`)
+        if(input.number == 7 )
+         channel.ack(message);        
+        })
+        console.log("Waiting for messages...")   
+    }
+    catch(err){
+        console.error(err)
+    }
+
+
+}
